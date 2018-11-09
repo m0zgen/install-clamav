ea52053783acdcd73b5899aa9dfcf325685446b1
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2018-11-09 21:21:32 +0600

    Clamscan daily added

---
+++ clamscan-daily.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Created by Yevgeniy Goncharov, https://sys-adm.in
+# Install daily ClamAV scan
+
+# Envs
+# ---------------------------------------------------\
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+touch /var/log/clamav.log
+
+cat >> /etc/cron.daily/clamscan_daily <<_EOF_
+#!/bin/bash
+# Envs
+# ---------------------------------------------------\
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+# Vars
+# ---------------------------------------------------\
+SUBJECT="`hostname` PASSED DAILY SCAN"
+EMAIL="my@mail.ru"
+LOG=/var/log/clamav.log
+TMP_LOG=/tmp/clam.daily
+SERVER_NAME=`hostname`
+#
+# ---------------------------------------------------\
+av_report() {
+
+    if [ `cat ${TMP_LOG}  | grep Infected | grep -v 0 | wc -l` != 0 ]
+    then
+      SUBJECT="[WARNING] `hostname` PASSED DAILY SCAN"
+    fi
+
+    EMAILMESSAGE=`mktemp /tmp/virus-alert.XXXXX`
+    echo "To: ${EMAIL}" >>  ${EMAILMESSAGE}
+    echo "From: clamavalert@${SERVER_NAME}" >>  ${EMAILMESSAGE}
+    echo "Subject: ${SUBJECT}" >>  ${EMAILMESSAGE}
+    echo "Importance: High" >> ${EMAILMESSAGE}
+    echo "X-Priority: 1" >> ${EMAILMESSAGE}
+    echo "`tail -n 50 ${TMP_LOG}`" >> ${EMAILMESSAGE}
+    sendmail -t < ${EMAILMESSAGE}
+
+  cat ${TMP_LOG} >> ${LOG}
+  rm -rf ${TMP_LOG}
+}
+
+av_scan() {
+  touch ${TMP_LOG}
+  nice -n15 clamscan -r / --exclude-dir=/sys/ --quiet --infected --log=${TMP_LOG}
+}
+
+av_scan
+av_report
+_EOF_
+
+chmod +x /usr/local/clamav/script/clamscan_daily
