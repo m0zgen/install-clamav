7b4a12daad35aa1475cee6d4e69d161a8f9da7a4
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2018-11-10 13:42:51 +0600

    Update installer script

--- install-sheduler.sh
+++ install-sheduler.sh
@@ -2,12 +2,21 @@
 # Created by Yevgeniy Goncharov, https://sys-adm.in
 # Install daily ClamAV scan
 
-# Envs
+# Envs /Functions
 # ---------------------------------------------------\
 PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
+Info() {
+  printf "\033[1;32m$@\033[0m\n"
+}
+
 # Install
 # ---------------------------------------------------\
-cp $SCRIPT_PATH/sheduler/clamscan-daily /etc/cron.daily/
+cp -u $SCRIPT_PATH/sheduler/clamscan-daily /etc/cron.daily/
 chmod +x /etc/cron.daily/clamscan-daily
+
+Info "Done!"
+Info "Scheduler installed to /etc/cron.daily/ folder"
+
+Info "You must change email receiver and sender in the /etc/cron.daily/clamscan-daily script!"
